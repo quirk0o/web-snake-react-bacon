80d8282cf2dcf32189e7a571d022377dd94d0531
miciek <miciek@gmail.com>
2015-09-25 00:00:59 +0200

    initial empty version

--- src/Vector.js
+++ src/Vector.js
@@ -1,33 +1,29 @@
 export default class Vector {
-    constructor(x, y) {
-      this.x = x
-      this.y = y
-    }
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
 
-    equals(p) {
-        return this.x === p.x && this.y === p.y
-    }
+  equals(p) {
+    return this.x === p.x && this.y === p.y
+  }
 
-    add(p) {
-        return new Vector(this.x + p.x, this.y + p.y)
-    }
+  add(p) {
+    return new Vector(this.x + p.x, this.y + p.y)
+  }
 
-    mod(size) {
-        return new Vector((this.x + size.x) % size.x, (this.y + size.y) % size.y)
-    }
+  static random(size) {
+    return new Vector(
+      Math.floor(Math.random() * size.x),
+      Math.floor(Math.random() * size.y)
+    )
+  }
 
-    static random(size) {
-        return new Vector(
-            Math.floor(Math.random() * size.x),
-            Math.floor(Math.random() * size.y)
-        )
-    }
+  static rotateRight(pos) {
+    return new Vector(-pos.y, pos.x)
+  }
 
-    static rotateRight(pos) {
-        return new Vector(-pos.y, pos.x)
-    }
-
-    static rotateLeft(pos) {
-        return new Vector(pos.y, -pos.x)
-    }
+  static rotateLeft(pos) {
+    return new Vector(pos.y, -pos.x)
+  }
 }
