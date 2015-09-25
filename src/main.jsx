c8bcf2cb47f6797e3ae8c4e6222094efb31112b4
miciek <miciek@gmail.com>
2015-09-25 13:54:24 +0200

    added instructions for the workshop

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+// 0. Intro
+// - Vector
+// - style.css: flexbox FTW
+// - index.html: styles are in bundle
+
+// 1. Static board
+// - props: API, component is a function
+// - React warnings in class and classNames and keys
+// - classNames function
+
+// 2. SnakeGame
+// - composition
+// - state: component is still a function
+
+// 3. Snake's moving
+// - component lifecycle
+// - ticks stream + random fruit
+// - defaultProps: pos and dir
+// - scan marble
+
+// 4. Snake's changing direction
+// - keys stream
+// - filter marble
+// - inputStreams
+// - map marble
+// - merge marble
+// - scan again: snake's head is moving on key press
+// - sampledBy marble
+
+// 5. Snake eats fruits
+// - score on fruitEaten
+// - slidingWindow?
+// - scan again: snake is getting bigger
